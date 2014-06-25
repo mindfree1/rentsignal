15ed82e1b041a10e32e0818daa6e8119e5e5f0b5
Trent <1mind2minds@gmail.com>
2014-06-25 23:22:44 +1000

    pushing changes to folder structure

--- www/childcare2014/user.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-</head>
-<body>
-	<div>
-		<p>This is the User page</p>
-	</div>
-</body>
-</html>
--- www/childcare2014v2/user.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-</head>
-<body style="background-color:#79D9FF;">
-	<div>
-		<p>This is the User page</p>
-	</div>
-</body>
-</html>
